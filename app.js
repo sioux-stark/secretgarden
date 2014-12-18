dc7c4338eba519f244cbf778209960cb13dbc47a
sandra stark <stark.sandy@gmail.com>
2014-12-18 11:52:27 -0800

    add files

---
+++ app.js
@@ -0,0 +1,274 @@
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  db = require("./models"),
+  passport = require("passport"),
+  session = require("cookie-session"),
+  app = express();
+  aws = require('aws-sdk'),
+  
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+/*
+  What is the session?
+  It is the object that lives in our app
+    and records relevant info about users
+    who are signed in
+*/
+
+app.use(session( {
+  secret: 'thisismysecretkey',
+  name: 'chocolate chip',
+  // this is in milliseconds
+  maxage: 3600000
+  })
+);
+
+// get passport started
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*
+SERIALizING
+Turns relevant user data into a string to be 
+  stored as a cookie
+*/
+passport.serializeUser(function(user, done){
+  console.log("SERIALIZED JUST RAN!");
+
+  done(null, user.id);
+});
+
+
+/*
+DeSERIALizing
+Taking a string and turns into an object
+  using the relevant data stored in the session
+*/
+passport.deserializeUser(function(id, done){
+  console.log("DESERIALIZED JUST RAN!");
+  db.user.find({
+      where: {
+        id: id
+      }
+    })
+    .then(function(secret){
+      done(null, secret);
+    },
+    function(err) {
+      done(err, null);
+    });
+});
+
+// WHEN SOMEONE WANTS THE SIGNUP PAGE
+app.get("/sign_up", function (req, res) {
+  res.render("users/sign_up");
+});
+
+// WHEN SOMEONE  SUBMITS A SIGNUP PAGE
+app.post("/users", function (req, res) {
+  var newUser = req.body.user;
+  console.log("New User:", newUser);
+  // CREATE a user and secure their password
+  db.user.createSecure(newUser.email, newUser.password, 
+    function () {
+      // if a user fails to create make them signup again
+      res.redirect("/sign_up");
+    },
+    function (err, user) {
+      // when successfully created log the user in
+      // req.login is given by the passport
+      req.login(user, function(){
+        // after login redirect show page
+        console.log("Id: ", user.id)
+        res.redirect('/users/' + user.id);
+      });
+    })
+});
+
+//homepage search areacode for gardens
+app.get('/', function (req,res) {
+  res.render('site/index.ejs');
+});
+
+//about page
+app.get('/about', function (req,res) {
+  res.render('site/about.ejs');
+});
+
+
+
+//gardens in the area
+// app.get('/gardens', function (req,res){
+//   //anytime i want catch something from database use params
+//   db.gardens.findAll({
+//     where: {zipcode:req.params.zipcode},
+//     attributes: url,
+//     include: [{model: user, attributes: userId}]
+//   .then(function(gardens){
+//   res.render('site/gardens.ejs'), {user: req.user, gardens:url}
+//   })
+// });
+
+//gardens in the area
+app.get("/gardens", function (req, res) {
+  var zipcode = req.query.zipcode;
+  console.log(zipcode)
+  db.user.findAll({ 
+    where: {
+      zipcode: zipcode
+    },
+    include: [db.garden] 
+  })
+  .then(function (matchedUsers) {
+    //res.send(matchedUsers);
+    res.render("site/gardens", {
+      users: matchedUsers
+    }); 
+  },
+  function (err){
+    console.log(new Error(err));
+    res.render("site/error");
+  });
+});
+
+
+
+//user garden
+app.get('/garden', function (req,res){
+  res.render('site/garden.ejs');
+});
+
+app.get('/gardenpictures',function (req,res){
+  res.render('users/gardenUpload')
+});
+
+//this is where user uploads and adds areacode
+app.get('/users/:id', function (req, res) {
+ res.render('users/gardenUpload', {userId: req.params.id});
+});
+
+//save zipcode to database
+app.post("/users/:id", function (req, res){
+  var addZip = req.body.user.zipcode;
+  console.log("ADDZIP", addZip)
+   db.user.find(req.params.id).success(function(user){
+    user.updateAttributes({zipcode: addZip}).then(function(user){
+      console.log(user)
+      res.redirect('/users/' + user.id);
+    })
+  });
+}); 
+
+// //adding description
+// app.post("/users/:id", function (req,res){
+//   var addDescription = req.body.garden.description;
+//   console.log("addDescription", addDescription)
+//   db.user.find(req.params.id).success(function(garden){
+//     user.updateAttributes({description: addDescription}).then(function(user){
+//       console.log(description)
+//       res.redirect('/users/' + user.id);
+//     })
+//   });
+// }); 
+
+
+// When someone wants the login page
+app.get("/login", function (req, res) {
+  res.render("users/login");
+});
+
+// Temporary redirect to users profile
+app.get('/temp', function (req, res) {
+  res.redirect('/users/' + req.user.id);
+});
+
+// Authenticating a user
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/temp',
+  failureRedirect: '/login'
+}));
+
+
+// app.post('users/:id', function (req, res) {
+//   var userId = req.params.id;
+//   var zipcode = req.body.user['zipcode'];
+//   // add a zipcode to their row in the usertable
+//   db.user
+//     .find(userId)
+//     .success( function (foundUser) {
+//       user.save().success(function(){zipcode});
+//       res.render('site/index'); 
+//     })
+//     .catch ( function (err) {
+//       console.log(err);
+//     });
+// });
+
+
+/// TESTING FILE UPLOAD
+var multer = require('multer');
+var done = false;
+
+app.use(function(req, res, next) {
+    console.log("RUNNING MULTER")
+    console.log(req.headers)
+    console.log(req.user)
+    var contentType = req.get("content-type");
+    if (contentType && contentType.match(/^multipart\/form\-data\;/) && req.user) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+},multer({ dest: __dirname + '/public/photos/',
+    rename: function(fieldname, filename) {
+      return filename + Date.now();
+    },
+    onFileUploadStart: function (file) {
+      console.log(file.originalname + ' ATTEMPTED');
+      console.log("CHECKING FOR CURRENT USER");
+      console.log(file)
+    },
+    onFileUploadComplete: function (file) {
+      console.log(file.fieldname + ' uploaded to ' + file.path);
+      done = true;
+    }
+}));
+// app.use(
+//   multer({ dest: __dirname + '/public/photos/',
+//     rename: function(fieldname, filename) {
+//       return filename + Date.now();
+//     },
+//     onFileUploadStart: function (file) {
+//       console.log(file.originalname + ' ATTEMPTED');
+//       console.log("CHECKING FOR CURRENT USER");
+//       console.log(file)
+//     },
+//     onFileUploadComplete: function (file) {
+//       console.log(file.fieldname + ' uploaded to ' + file.path);
+//       done = true;
+//     }
+// }));
+
+app.post('/fileupload', function(req, res){
+    console.log(req.files);
+    db.garden.create({
+      title: req.files.userPhoto.name,
+      userId: req.user.id
+    }).then(function (garden) {
+      res.redirect("/")
+    });
+});
+
+
+
+ 
+
+ 
+
+
+app.listen(3000, function () {
+  console.log("LISTENING");
+})
---
+++ migrations/20141214163320-create-user.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        type: DataTypes.STRING
+      },
+      email: {
+        type: DataTypes.STRING
+      },
+      password: {
+        type: DataTypes.STRING
+      },
+      lattitude: {
+        type: DataTypes.STRING
+      },
+      longitude: {
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      zipcode: {
+        type: DataTypes.STRING
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    }).done(done);
+  },
+  down: function(migration, DataTypes, done) {
+    migration.dropTable("users").done(done);
+  }
+};
